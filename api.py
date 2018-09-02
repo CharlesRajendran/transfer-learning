4af6739b74b5298d7680d789a76ee3e3177f79a2
CharlesRajendran <charlesrajendran44@gmail.com>
2018-09-02 14:37:01 +0530

    upload image api is done

--- api.py
+++ api.py
@@ -10,10 +10,37 @@ import label_image as li
 '''
 
 @get('/')
+def indexPage():
+    return '''
+        <html>
+            <head>
+                <title>Upload Image</title>
+            </head>
+            <body>
+                <form method="post" action="/upload" enctype="multipart/form-data">
+                    <input type="file" accept="image/png, image/jpeg" name="image" />
+                    <input type="submit" value="predict" />
+                </form>
+            </body>
+        </html>
+    '''
+
+@post('/upload')
+def uploadImage():
+    image = request.files.image
+    filename = "inference_image/image.jpg"
+    with open(filename,'wb') as open_file:
+        open_file.write(image.file.read())
+    open_file.close()
+    return get_top_predictions()
+
+
+
+#@get('/predictions')
 def get_top_predictions():
-    lable_index, labels_list, results_list = li.get_lables("bottle.jpg")
+    lable_index, labels_list, results_list = li.get_lables("image.jpg")
     selected_list = [{"class": labels_list[i], "probability": float(results_list[i]) } for i in lable_index]
     return {"predictions": selected_list}
 
-#run(reloader=True,debug=True)
-run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
+run(reloader=True,debug=True)
+# run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
